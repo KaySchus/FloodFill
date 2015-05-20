d8624fabc426d09dfc85cd566a20e5f6443020c9
KaySchus <ksschu06@louisville.edu>
2015-05-20 15:49:34 +0000

    Moved images to seperate output folder

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
+import sys
 import grid
 import generator
 
-test = grid.Grid(256, 256, grid.ImageWriter("bars.png"), generator.BoxGenerator())
+image_title = sys.argv[1]
+
+test = grid.Grid(256, 256, grid.ImageWriter("images/" + image_title), generator.BoxGenerator())
 test.generate()
 test.output()
 

4596ea2e890381a7930f5c3df0f6cbce22bda0c9
KaySchus <ksschu06@louisville.edu>
2015-05-20 15:41:29 +0000

    Sanitizing build

--- generator.py
+++ generator.py
@@ -11,7 +11,7 @@ class BoxGenerator(Generator):
 	def generate(self, grid):
 		for i in range(grid.height):
 			for j in range(grid.width):
-				if ((j == 0) or (i == 0)):
+				if ((j == 0) or (i == 0) or (j == grid.width - 1) or (i == grid.height - 1)):
 					grid.grid[i][j] = Cell.WALL
 				else:
 					grid.grid[i][j] = Cell.BLANK

d0174d5a990e34fc329bec2b76e09056bc04c6bf
KaySchus <ksschu06@louisville.edu>
2015-05-21 16:00:21 +0000

    DOES NOT WORK, DO NOT RUN

--- Simulation.py
+++ Simulation.py
@@ -10,6 +10,16 @@ class FloodSimulator:
 
 		print location
 
+		loop_count = 0
+		filename = self.grid.writer.filename
+		filename_parts = filename.split('.')
+
+		while (location != -1):
+			location = self.take_turn(location)
+			self.grid.writer.filename = filename_parts[0] + "_" + str(loop_count) + "." + filename_parts[1]
+			self.grid.output()
+			loop_count += 1
+
 	def find_start(self):
 		for i in range(self.grid.height):
 			for j in range(self.grid.width):
@@ -17,3 +27,30 @@ class FloodSimulator:
 					return (i, j)
 
 		return (-1, -1)
+
+	def take_turn(self, location):
+		north = self.grid.grid[location[0] + 1][location[1]]
+		south = self.grid.grid[location[0] - 1][location[1]]
+		east = self.grid.grid[location[0]][location[1] + 1]
+		west = self.grid.grid[location[0]][location[1] - 1]
+
+		if (north == Cell.BLANK):
+			north = Cell.RED
+			location += (1, 0)
+
+		elif (east == Cell.BLANK):
+			east = Cell.RED
+			location += (0, 1)
+		
+		elif (south == Cell.BLANK):
+			south = Cell.RED
+			location += (-1, 0)
+
+		elif (west == Cell.BLANK):
+			west = Cell.RED
+			location += (0, -1)
+
+		else:
+			return -1
+
+		return location
